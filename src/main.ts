92246822d53e650f67bf109b78b8f3bd38714c4d
Alex Malyk <oleksiimal@wizardsdev.com>
2024-03-18 00:55:41 +0200

    fix main ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
-import { NextFunction } from 'express';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:1420', // Specify the allowed origin 
-    methods: 'GET,POST', // Allowed methods
+    origin: 'http://localhost:1420', // Specify the allowed origin
     credentials: true,
-    allowedHeaders: 'Content-Type, Authorization' 
+    allowedHeaders: 'Content-Type, Authorization'
   });
   app.use(cookieParser());
   await app.listen(3000);
